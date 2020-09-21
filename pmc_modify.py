b8184cfba356453023cf88d62465a6159906cc6d
prafagr <prafagr@gmail.com>
2020-09-21 19:49:31 +0530

    Update pmc_modify.py

--- pmc_modify.py
+++ pmc_modify.py
@@ -4,7 +4,7 @@ from procmon_parser import load_configuration, dump_configuration, Rule
 
 with open("C:\\Users\\test_sym\\Desktop\\initiative\\1.pmc", "rb")as f:
   config = load_configuration(f)
-new_rules = [ Rule('User', 'is_not', '##username', 'include'), Rule('Process_name', 'contains', '##processname', 'include'), Rule('Operation', 'is', 'WriteFile', 'include')]
+new_rules = [ Rule('Process_name', 'contains', '##processname', 'include'), Rule('Operation', 'is', 'WriteFile', 'include')]
 config["FilterRules"] = new_rules + config["FilterRules"]
 
 with open("C:\\Users\\test_sym\\Desktop\\initiative\\2.pmc", "wb")as f:
